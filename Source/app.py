ecfcde2fb48d5743bc0d375d93248e1e37c1e689
AdithyanandanArun <adithyanandan.aavani@gmail.com>
2026-03-02 20:02:55 +0530

    Basic structure and app commit

---
+++ Source/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+apikey = os.getenv("groqapi")
+client = Groq(api_key=apikey)
+app = Flask(__name__)
+
+@app.route ("/")
+def index():
+    return render_template("index.html")
+@app.route ("/groq", methods=["post"])
+def groq():
+    user_message = request.json.get("message")
+
+    response = client.chat.completions.create(model="llama-3.3-70b-versatile",messages=[{"role": "system", "content": "You are a assistant."},{"role": "user", "content": user_message}])
+
+    reply = response.choices[0].message.content
+    return jsonify({"reply": reply})
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
